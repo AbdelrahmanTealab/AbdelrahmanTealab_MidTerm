f89cdc0f727d52f105d3eed92905c7711746490d
Abdelrahman Tealab <62073015+AbdelrahmanTealab@users.noreply.github.com>
2020-10-27 16:22:43 -0400

    adjusted variable namings and added cancel button

--- AbdelrahmanTealab_MidTerm/ViewController.swift
+++ AbdelrahmanTealab_MidTerm/ViewController.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet var listName: UITextField!
     /*----------------------------------------------
-    The item name labels currently have no usage at all
-    but i connected them anyway incase there is a future
-    usage for them.
-    ----------------------------------------------*/
+     The item name labels currently have no usage at all
+     but i connected them anyway incase there is a future
+     usage for them.
+     ----------------------------------------------*/
     @IBOutlet weak var itemName0: UITextField!
     @IBOutlet weak var itemName1: UITextField!
     @IBOutlet weak var itemName2: UITextField!
@@ -22,9 +22,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var itemName4: UITextField!
     @IBOutlet weak var itemName5: UITextField!
     /*----------------------------------------------
-    Quantity labels are the labels that show the value
-    of the steppers when incrementing or decrementing.
-    ----------------------------------------------*/
+     Quantity labels are the labels that show the value
+     of the steppers when incrementing or decrementing.
+     ----------------------------------------------*/
     @IBOutlet weak var quantityLabel0: UILabel!
     @IBOutlet weak var quantityLabel1: UILabel!
     @IBOutlet weak var quantityLabel2: UILabel!
@@ -32,10 +32,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var quantityLabel4: UILabel!
     @IBOutlet weak var quantityLabel5: UILabel!
     /*----------------------------------------------
-    The Purpose of the stepper outlets is only for
-    the cancel button so that i can reset their values
-    back to 0.0
-    ----------------------------------------------*/
+     The Purpose of the stepper outlets is only for
+     the cancel button so that i can reset their values
+     back to 0.0
+     ----------------------------------------------*/
     @IBOutlet weak var stepperOutlet0: UIStepper!
     @IBOutlet weak var stepperOutlet1: UIStepper!
     @IBOutlet weak var stepperOutlet2: UIStepper!
@@ -43,64 +43,88 @@ class ViewController: UIViewController {
     @IBOutlet weak var stepperOutlet4: UIStepper!
     @IBOutlet weak var stepperOutlet5: UIStepper!
     /*----------------------------------------------
-    The arrays below are label and stepper arrays
-    they will store the above outlets so that i can
-    easily loop them and manipulate their values.
-    ----------------------------------------------*/
-    var labelsArray = [Int:UILabel]()
-    var steppersArray = [Int:UIStepper]()
-
+     The dictionaries below are label and stepper
+     dictionary they will store the above outlets so
+     that i can easily loop them and manipulate their
+     values.
+     ----------------------------------------------*/
+    var textsDict = [Int:UITextField]()
+    var labelsDict = [Int:UILabel]()
+    var steppersDict = [Int:UIStepper]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         /*----------------------------------------------
-        In viewDidLoad, i populate the arrays with the
-        outlets and then call function reset().
-        ----------------------------------------------*/
-        labelsArray[0]=quantityLabel0
-        labelsArray[1]=quantityLabel1
-        labelsArray[2]=quantityLabel2
-        labelsArray[3]=quantityLabel3
-        labelsArray[4]=quantityLabel4
-        labelsArray[5]=quantityLabel5
+         In viewDidLoad, i populate the dictionaries with
+         the outlets and then call function reset().
+         ----------------------------------------------*/
+        textsDict[0]=itemName0
+        textsDict[1]=itemName1
+        textsDict[2]=itemName2
+        textsDict[3]=itemName3
+        textsDict[4]=itemName4
+        textsDict[5]=itemName5
         
-        steppersArray[0]=stepperOutlet0
-        steppersArray[1]=stepperOutlet1
-        steppersArray[2]=stepperOutlet2
-        steppersArray[3]=stepperOutlet3
-        steppersArray[4]=stepperOutlet4
-        steppersArray[5]=stepperOutlet5
+        labelsDict[0]=quantityLabel0
+        labelsDict[1]=quantityLabel1
+        labelsDict[2]=quantityLabel2
+        labelsDict[3]=quantityLabel3
+        labelsDict[4]=quantityLabel4
+        labelsDict[5]=quantityLabel5
+        
+        steppersDict[0]=stepperOutlet0
+        steppersDict[1]=stepperOutlet1
+        steppersDict[2]=stepperOutlet2
+        steppersDict[3]=stepperOutlet3
+        steppersDict[4]=stepperOutlet4
+        steppersDict[5]=stepperOutlet5
         
         reset()
+        
     }
     
     func reset(){
         /*----------------------------------------------
-        This function will be called when Cancel button
-        is pressed, it will basically reset the values
-        of the steppers and labels.
-        ----------------------------------------------*/
-        for label in labelsArray {
+         This function will be called when Cancel button
+         is pressed, it will basically reset the values
+         of the steppers, labels, textfields and list name.
+         ----------------------------------------------*/
+        for label in labelsDict {
             label.value.text = "0"
         }
-        for stepper in steppersArray {
+        for stepper in steppersDict {
             stepper.value.value = 0.0
         }
+        for texts in textsDict {
+            texts.value.text = nil
+        }
+        listName.text = "Shopping List" //by default
     }
-
+    
     @IBAction func stepperPressed(_ sender: UIStepper) {
         /*----------------------------------------------
-        When incrementing or decrementing the steppers,
-        i make use of the 'Tag' attribute to identify
-        which stepper was pressed, then i adjust the
-        label associated with the same tag number in the
-        labels array.
-        ----------------------------------------------*/
+         When incrementing or decrementing the steppers,
+         i make use of the 'Tag' attribute to identify
+         which stepper was pressed, then i adjust the
+         label associated with the same tag number in the
+         labels dictionary.
+         ----------------------------------------------*/
         let stepperTag = sender.tag
         let stepperValue = String(Int(sender.value))
         
-        labelsArray[stepperTag]?.text = stepperValue
+        labelsDict[stepperTag]?.text = stepperValue
     }
-
     
+    @IBAction func cancelPressed(_ sender: UIButton) {
+        /*----------------------------------------------
+         Cancel button calls the function reset() above.
+         ----------------------------------------------*/
+        reset()
+    }
+    
+    @IBAction func savePressed(_ sender: UIButton) {
+        /*----------------------------------------------
+         For future.
+         ----------------------------------------------*/
+    }
 }
-
